bf2ddf01a9328c9a5dbea55adcbcdf9cdeb3e153
강지원 <kangji149@naver.com>
2024-07-07 18:52:41 +0900

    init : multi module setting

---
+++ application/app-api/src/main/java/core/startup/mealtoktok/MealTokTokApplication.java
@@ -0,0 +1,13 @@
+package core.startup.mealtoktok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MealTokTokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealTokTokApplication.class, args);
+    }
+}
