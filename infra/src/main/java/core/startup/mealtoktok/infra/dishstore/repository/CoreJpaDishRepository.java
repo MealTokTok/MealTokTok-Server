e4a95172a52066be6310c3db3e2e898e25cee650
강지원 <kangji149@naver.com>
2024-07-31 18:30:47 +0900

    build : 구글 컨벤션 및 spotless 적용
    - task를 통해 커밋 전에 구글 컨벤션 체크 및 적용

--- infra/src/main/java/core/startup/mealtoktok/infra/dishstore/repository/CoreJpaDishRepository.java
+++ infra/src/main/java/core/startup/mealtoktok/infra/dishstore/repository/CoreJpaDishRepository.java
@@ -17,53 +17,57 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CoreJpaDishRepository implements DishRepository {
 
-    private final JpaDishRepository jpaDishRepository;
-    private final JpaDishStoreRepository jpaDishStoreRepository;
-    private final JpaDishCategoryRepository jpaDishCategoryRepository;
+  private final JpaDishRepository jpaDishRepository;
+  private final JpaDishStoreRepository jpaDishStoreRepository;
+  private final JpaDishCategoryRepository jpaDishCategoryRepository;
 
-    @Override
-    public void save(DishStore dishStore, DishCategory dishCategory, DishInfo dishInfo) {
-        DishStoreEntity dishStoreEntity = jpaDishStoreRepository.getReferenceById(dishStore.getStoreId());
-        DishCategoryEntity dishCategoryEntity = jpaDishCategoryRepository.getReferenceById(dishCategory.getCategoryId());
-        jpaDishRepository.save(DishEntity.of(dishStoreEntity, dishCategoryEntity, dishInfo));
-    }
+  @Override
+  public void save(DishStore dishStore, DishCategory dishCategory, DishInfo dishInfo) {
+    DishStoreEntity dishStoreEntity = jpaDishStoreRepository.getReferenceById(
+        dishStore.getStoreId());
+    DishCategoryEntity dishCategoryEntity = jpaDishCategoryRepository.getReferenceById(
+        dishCategory.getCategoryId());
+    jpaDishRepository.save(DishEntity.of(dishStoreEntity, dishCategoryEntity, dishInfo));
+  }
 
-    @Override
-    public Dish findCategoryById(TargetDish targetDish) {
-        return jpaDishRepository.findById(targetDish.dishId())
-                .map(DishEntity::toDomain)
-                .orElseThrow(()-> DishNotFoundException.EXCEPTION);
-    }
+  @Override
+  public Dish findCategoryById(TargetDish targetDish) {
+    return jpaDishRepository.findById(targetDish.dishId())
+        .map(DishEntity::toDomain)
+        .orElseThrow(() -> DishNotFoundException.EXCEPTION);
+  }
 
-    @Override
-    public void delete(Dish dish) {
-        DishEntity dishEntity = jpaDishRepository.getReferenceById(dish.getDishId());
-        jpaDishRepository.delete(dishEntity);
-    }
+  @Override
+  public void delete(Dish dish) {
+    DishEntity dishEntity = jpaDishRepository.getReferenceById(dish.getDishId());
+    jpaDishRepository.delete(dishEntity);
+  }
 
-    @Override
-    public List<Dish> findAllByStoreAndCategory(DishStore dishStore, DishCategory dishCategory) {
-        return jpaDishRepository.findAllByStoreAndCategory(dishStore.getStoreId(), dishCategory.getCategoryId())
-                .stream().map(DishEntity::toDomain)
-                .toList();
-    }
+  @Override
+  public List<Dish> findAllByStoreAndCategory(DishStore dishStore, DishCategory dishCategory) {
+    return jpaDishRepository.findAllByStoreAndCategory(dishStore.getStoreId(),
+            dishCategory.getCategoryId())
+        .stream().map(DishEntity::toDomain)
+        .toList();
+  }
 
-    @Override
-    public void update(Dish dish, DishInfo dishInfo) {
-        DishEntity dishEntity = jpaDishRepository.getReferenceById(dish.getDishId());
-        dishEntity.update(dishInfo);
-    }
+  @Override
+  public void update(Dish dish, DishInfo dishInfo) {
+    DishEntity dishEntity = jpaDishRepository.getReferenceById(dish.getDishId());
+    dishEntity.update(dishInfo);
+  }
 
-    @Override
-    public boolean existsByDishStoreEntityAndDishName(DishStore dishStore, String dishName) {
-        DishStoreEntity dishStoreEntity = jpaDishStoreRepository.getReferenceById(dishStore.getStoreId());
-        return jpaDishRepository.existsByDishStoreEntityAndDishName(dishStoreEntity, dishName);
-    }
+  @Override
+  public boolean existsByDishStoreEntityAndDishName(DishStore dishStore, String dishName) {
+    DishStoreEntity dishStoreEntity = jpaDishStoreRepository.getReferenceById(
+        dishStore.getStoreId());
+    return jpaDishRepository.existsByDishStoreEntityAndDishName(dishStoreEntity, dishName);
+  }
 
-    @Override
-    public DishCategory findCategoryById(TargetDishCategory targetDishCategory) {
-        return jpaDishCategoryRepository.findById(targetDishCategory.categoryId())
-                .map(DishCategoryEntity::toDomain)
-                .orElseThrow(() -> DishCategoryNotFoundException.EXCEPTION);
-    }
+  @Override
+  public DishCategory findCategoryById(TargetDishCategory targetDishCategory) {
+    return jpaDishCategoryRepository.findById(targetDishCategory.categoryId())
+        .map(DishCategoryEntity::toDomain)
+        .orElseThrow(() -> DishCategoryNotFoundException.EXCEPTION);
+  }
 }
