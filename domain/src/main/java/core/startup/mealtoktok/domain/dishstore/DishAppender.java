9541d79b43ddf408a8c8e45ad88d1fcf20c4b857
Somin Park <ps4708@naver.com>
2024-08-06 22:03:22 +0900

    refactor: 반찬 생성, 수정 관련 비즈니스 로직 수정

--- domain/src/main/java/core/startup/mealtoktok/domain/dishstore/DishAppender.java
+++ domain/src/main/java/core/startup/mealtoktok/domain/dishstore/DishAppender.java
@@ -8,9 +8,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class DishAppender {
 
+    private final DishValidator dishValidator;
     private final DishRepository dishRepository;
 
     public void append(DishStore dishStore, DishCategory dishCategory, DishInfo dishInfo) {
+        dishValidator.validateName(dishStore, dishInfo.dishName());
         dishRepository.save(dishStore, dishCategory, dishInfo);
     }
 }
